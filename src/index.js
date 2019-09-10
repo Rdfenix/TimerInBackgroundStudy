c9369147ffb6bb1ba9e55304e9b3c734f51cade2
rdfenix <rhoodsk8erboy@gmail.com>
2019-09-10 16:28:41 -0300

    adding sagas

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import AppRouter from './routes';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-
+import createSagasMiddleware from 'redux-saga';
+import rootSaga from './Sagas';
 import RootReducer from './Reducer';
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const create = () => {
+  const devTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = applyMiddleware()(createStore)(RootReducer, devTools);
+  const sagaMiddleware = createSagasMiddleware();
+  const middleware = applyMiddleware(sagaMiddleware)(createStore)(
+    RootReducer,
+    devTools,
+  );
+
+  sagaMiddleware.run(rootSaga);
+
+  return middleware;
+};
+
+const store = create();
 
 const App = () => {
   return (
